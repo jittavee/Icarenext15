85f8fbff75eeab86d2c8886080b86023225efb0e
jittavee <vcopcom@gmail.com>
2025-06-05 14:24:51 +0700

    icare_1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Hero from '@/components/Hero';
 import CompanyLogos from '@/components/CompanyLogos';
-
+import AboutUs from '@/components/AboutUs';
+import NewsUpdate from '@/components/NewsUpdate';
+import Resume from '@/components/Resume';
 
 export default function page() {
   return (
   <main>
     <Hero/>
     <CompanyLogos/>
+    <AboutUs/>
+    <NewsUpdate/>
+    <Resume />
   </main>
     
   )
